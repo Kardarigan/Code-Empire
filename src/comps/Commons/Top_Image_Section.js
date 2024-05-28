074ffe4496de5c8d54bee5daaed78fce80f99877
D <htehran333@gmail.com>
2024-05-28 12:28:20 -0700

    Top image component done

---
+++ src/comps/Commons/Top_Image_Section.js
@@ -0,0 +1,12 @@
+const Top_Image_Section = ({ title = null, label = null, describe, cover }) => {
+  return (
+    <section>
+      <img src={cover} alt={title} className="w-full rounded h-auto" />
+      {label && <p className="label">{label}</p>}
+      {title && <h2 className="title">{title}</h2>}
+      <p className="para">{describe}</p>
+    </section>
+  );
+};
+
+export default Top_Image_Section;
